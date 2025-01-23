6e9a5a67d2e236027bc2de034629c28f4941a413
jalknThoth <alejalkndro@gmail.com>
2025-01-23 04:36:57 -0500

    updated

---
+++ models/races/0108/caption.py
@@ -0,0 +1,26 @@
+"""
+.
+AMRAP 25mins
+5 Sets
+8 Burpees
+10 Snatch 30/40kg
+12 Box Jump Overs 20/24"
+.
+.
+.
+Mobility
+hold for 1min
+frog
+saddle
+pigeon
+Seated straddle
+Reclined Butterfly
+Pretzel stretch
+Thoracic Spine/Shoulders
+Wide legged child's pose
+Thread the needle pose
+.
+.
+.
+#crosstraining #fitness #crossfit #workout #gym #training #wod #motivation #fit #fitnessmotivation #sport #functionaltraining #crossfitgirls #personaltrainer #cardio #cross #weightlifting #crossfitlife #instafit #treino #saude #running #fitfam #coach #powerlifting #gymlife #crossfitbrasil #strength #crossfitter #bodybuilding #frogpose #saddlepose #pigeonpose #seatedstraddle #reclinedbutterfly #pretzelstretch #thoracicspine #shoulders #wideleggedchildspose #threadtheneedlepose
+"""
